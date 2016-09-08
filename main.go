c3adc011a290fa091f709b90cd05629bd5e0e73d
junjun1092 <zhaojunjun0531@126.com>
2016-09-08 23:10:43 +0800

    学习go中struct的值传递和地址传递

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
+
 import (
-	_ "beego-blog/routers"
-	"github.com/astaxie/beego"
+	//_ "beego-blog/routers"
+	"fmt"
 )
 
-func main() {
-	beego.Run()
+type user struct {
+	Name string
+	Age int
 }
+
+func main() {
+	//beego.Run()
+	//匿名结构
+	//a := &struct {
+	//	Name string
+	//	Age int
+	//}{
+	//	Name : "Joe",
+	//	Age:19,
+	//}
+	a := &user{Name:"Lucy",Age:19}
+	A(a)
+	fmt.Print(a)//Lucy 19，这里修改的是它的副本，没有修改原对象
+}
+func A(user *user){
+	user.Name = "Lily"
+}
+
+
 
